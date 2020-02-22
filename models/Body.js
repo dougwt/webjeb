a675b7393b430999bbcf41efe23ea1d7310499dd
Doug Tyler <dougwt@gmail.com>
2020-02-22 14:23:09 -0800

    WIP Body schema

--- models/Body.js
+++ models/Body.js
@@ -1,27 +1,41 @@
 const mongoose = require('mongoose');
+const appConfig = require('../lib/appConfig');
 
 const { Schema } = mongoose;
 
-const bodySchema = new Schema({
-  name: String,
-  moons: [
-    {
-      moon: String,
+const bodySchema = new Schema(
+  {
+    name: String,
+    moons: [
+      {
+        moon: String,
+        rel: String
+      }
+    ],
+    aroundBody: {
+      body: String,
       rel: String
-    }
-  ],
-  // mass: {
-  //   massValue: Number,
-  //   massExponent: Number
-  // },
-  // radius: Number,
-  // gravity: Number,
-  // isPlanet: Boolean,
-  // aroundBody: {
-  //   body: String,
-  //   rel: String
-  // },
-  rel: String
+    },
+    mass: {
+      massValue: Number,
+      massExponent: Number
+    },
+    radius: Number,
+    gravity: Number,
+    source: String,
+    rel: String
+  },
+  { timestamps: true }
+);
+
+bodySchema.virtual('isStar').get(function() {
+  return this.aroundBody === null;
+});
+bodySchema.virtual('isPlanet').get(function() {
+  return this.aroundBody && this.aroundBody.body === appConfig.centralBody;
+});
+bodySchema.virtual('isMoon').get(function() {
+  return !this.isStar && !this.isPlanet;
 });
 
 const Body = mongoose.model('bodies', bodySchema);
