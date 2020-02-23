59c2c363c3394539e46a3beae84bc6ababe25d48
Doug Tyler <dougwt@gmail.com>
2020-02-22 22:35:28 -0800

    Implement /api/bodies/index.js

--- api/bodies/index.js
+++ api/bodies/index.js
@@ -1,5 +1,46 @@
-module.exports = (req, res) => {
-  res.end(
-    'This API endpoint has not been implemented yet. Please check back later.'
-  );
-};
+const Body = require('../../models/Body');
+const { applyMiddleware, RequestError } = require('../../lib/applyMiddleware');
+const withMongoose = require('../../lib/withMongoose');
+const appConfig = require('../../lib/appConfig');
+
+module.exports = applyMiddleware([withMongoose], async (req, res) => {
+  try {
+    if (req.method !== 'GET') {
+      throw new RequestError(404, 'Unsupported request method');
+    }
+
+    console.log('Searching for bodies...');
+
+    let bodies = await Body.find({});
+
+    console.table(bodies);
+
+    bodies = bodies.map(
+      ({
+        name,
+        moons,
+        equatorialRadius,
+        mass,
+        surfaceGravity,
+        aroundBody,
+        source,
+        rel
+      }) => {
+        return {
+          name,
+          moons,
+          equatorialRadius,
+          mass,
+          surfaceGravity,
+          aroundBody,
+          source,
+          rel
+        };
+      }
+    );
+
+    return res.json(bodies);
+  } catch (error) {
+    throw new RequestError(500, 'Unable to query database');
+  }
+});
