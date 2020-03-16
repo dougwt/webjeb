5c90bd6aed7611ec24fb08e20b6a1dc33b2c5352
Doug Tyler <dougwt@gmail.com>
2020-03-15 23:30:44 -0700

    Add App.js stub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
+        <h1>webjeb</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className="hello">Hello</span>, and welcome to the experimental
+          test site for a small collection of handy calculators for use with
+          Kerbal Space Program. This site is powered by a{' '}
+          <a href="/api/">
+            publicly available API containing community-sourced info about the
+            Kerbol solar system
+          </a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          To begin, select a planetary body...
+          <select name="bodies" id="body-select">
+            <option value="">Loading...</option>
+          </select>
+        </p>
+
+        <p>
+          ...and a calculator...
+          <ul>
+            <li>
+              <a href="#">Required delta-v for Hohmann transfer</a>
+            </li>
+            <li>
+              <a href="#">Orbital period &amp; darkness time</a>
+            </li>
+          </ul>
+        </p>
       </header>
+
+      <form action="#">
+        <fieldset>
+          <legend>Delta-v for Hohmann transfer orbital changes</legend>
+
+          <p>
+            <label for="planet">Planetary body:</label>
+            <select name="planet" id="planet">
+              <option value="">Loading...</option>
+            </select>
+          </p>
+
+          <p>
+            <label for="lower">Lower altitude:</label>
+            <input type="text" id="lower" /> km
+          </p>
+
+          <p>
+            <label for="higher">Higher altitude:</label>
+            <input type="text" id="higher" /> km
+          </p>
+
+          <p>
+            <button>Submit</button>
+            <button type="reset">Clear</button>
+          </p>
+        </fieldset>
+      </form>
     </div>
   );
 }
